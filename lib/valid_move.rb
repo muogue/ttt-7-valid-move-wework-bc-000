878d1958ff0d54953b5651aee9092ff69abf7f0e
Vincent Huang <vincent.huang@wework.com>
2018-02-09 01:23:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  if (board[index] == nil)
+  if (index > 8)
     return false
   end
   return ("x".casecmp(board[index]) == 0 || "o".casecmp(board[index]) == 0)
