876555c13ef6af562e4371f8adf4985a3edbfa34
Vincent Huang <vincent.huang@wework.com>
2018-02-09 01:23:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  return !position_taken?(board,index)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if (index > 8)
+    return true
+  end
+  return ("x".casecmp(board[index]) == 0 || "o".casecmp(board[index]) == 0)
+end
